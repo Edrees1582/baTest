b994ca109cad18ff18c80a138340d428ba3e373b
Edrees Nabeel Ashab <edrees.ashab@outlook.com>
2023-11-29 14:10:09 +0300

    Added constructors to make the initialization proccess flexible

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -6,6 +6,16 @@ public class BankAccount {
         this.ownerName = ownerName;
         balance = 0.0;
     }
+	
+	public BankAccount() {
+        this.ownerName = "New Account" + (int) (Math.floor(Math.random() * 1000) + 1);
+        balance = 0.0;
+    }
+
+	public BankAccount(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
 
     public String getOwnerName() {
         return ownerName;
