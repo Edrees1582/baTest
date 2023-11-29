1e7c0a8cb619560fdf4336f8c1070a4a984be739
Edrees Nabeel Ashab <edrees.ashab@outlook.com>
2023-11-29 16:15:38 +0300

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount b1 = new BankAccount("Edrees");
-        BankAccount b2 = new BankAccount("Ahmad");
+        BankAccount b1 = new BankAccount("Edrees", 100.0);
+        BankAccount b2 = new BankAccount();
+
+        b1.withdraw(75.0);
+
+        b2.deposit(35.0);
+		b2.withdraw(50.0);
     }
 }
